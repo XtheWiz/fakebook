4f674318ffd75808c357257a4ef44906520b3d81
XtheWiz <xthewiz.dev@gmail.com>
2017-03-01 21:12:17 +0700

    Init project

---
+++ app/Post.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $table = "Post";
+
+    public function owner() {
+      return $this->belongTo(User::class, 'ownerID');
+    }
+
+    public function comments() {
+      return $this->hasMany(Comment::class, 'ownerPost');
+    }
+}
